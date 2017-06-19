802c2a5c190ef391e87d4f85059f690fc514b575
eli fridman <thisiselimail@gmail.com>
2017-06-19 21:25:17 +0300

    add todo app

--- app/app.jsx
+++ app/app.jsx
@@ -2,12 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 
 //Load css
 require('style!css!sass!applicationStyles');
 $(document).foundation();
 
 ReactDOM.render(
-  <p>boilerplate</p>,
+  <TodoApp/>,
   document.getElementById('app')
 );
